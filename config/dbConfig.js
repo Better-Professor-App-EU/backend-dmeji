98b5fabdde34f059f13530365f5a924b70a2ffed
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-19 07:44:11 +0000

    ran npx heroku migrate

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,6 +1,7 @@
+const knex = require("knex");
 
-const knex = require('knex')
+config = require("../knexfile.js");
 
-config = require('../knexfile')
+const dbEnv = process.env.DB_ENV || "development";
 
-module.exports = knex(config.development)
+module.exports = knex(config[dbEnv]);
