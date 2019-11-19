1f5607d4b58c604f02ba0c8874620f0c9985c4ed
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-19 10:14:10 +0000

    latest

--- database/seeds/001-seed-users.js
+++ database/seeds/001-seed-users.js
@@ -1,25 +1,21 @@
 const bcrypt = require("bcryptjs");
 
 exports.seed = function(knex) {
-  return knex("users")
-    .truncate()
-    .then(function() {
-      return knex("users").insert([
-        {
-          id: 1,
-          username: "Profsmalls",
-          password: bcrypt.hashSync("user1", 11)
-        },
-        {
-          id: 2,
-          username: "Profbigs",
-          password: bcrypt.hashSync("user2", 11)
-        },
-        {
-          id: 3,
-          username: "Profmediums",
-          password: bcrypt.hashSync("user3", 11)
-        }
-      ]);
-    });
+  return knex("users").insert([
+    {
+      id: 1,
+      username: "Profsmalls",
+      password: bcrypt.hashSync("user1", 11)
+    },
+    {
+      id: 2,
+      username: "Profbigs",
+      password: bcrypt.hashSync("user2", 11)
+    },
+    {
+      id: 3,
+      username: "Profmediums",
+      password: bcrypt.hashSync("user3", 11)
+    }
+  ]);
 };
