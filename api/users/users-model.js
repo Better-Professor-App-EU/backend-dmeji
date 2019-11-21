b010c9769f38bd46ce3d6331f79604f8340706a5
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-21 16:22:26 +0000

    latest

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,9 +1,23 @@
 const db = require("../../config/dbConfig");
 
 module.exports = {
-  findUsers
+  findUsers,
+  findBy,
+  add
 };
 
 function findUsers() {
   return db("users").select("id", "username");
 }
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+function add(user) {
+    return db("users")
+      .insert(user, "id")
+      .then(([id]) => {
+        return findById(id);
+      });
+  }
