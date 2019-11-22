06b2020d90f14fcd65112937829fc88587c03ec6
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-22 08:21:59 +0000

    latest

--- api/messages/messages-router.js
+++ api/messages/messages-router.js
@@ -10,7 +10,7 @@ const Messages = require("../messages/messages-model");
 
 const messagesRouter = express.Router();
 
-messagesRouter.get("/", restrict, validateUser, (req, res) => {
+messagesRouter.get("/", restrict, (req, res) => {
   Messages.findMessages()
     .then(messages => {
       messages.map(message => {
